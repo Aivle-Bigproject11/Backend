eaa755b31f8bff282c2ddd6347532ec3dd43c653
ohhyunjong <jong001029@naver.com>
2025-08-01 11:12:01 +0900

    250801/11:11_현종 1. h2데이터베이스에 자동으로 전환서비스 입력 sql 2. 각종 전환서비스 이미지들

---
+++ servicecontext/src/main/resources/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO conversion_service_table (service_id, detailed_url, image_url, service_name) VALUES
+  (1, 'https://naver.com', '/images/marry.jpg', '결혼정보 서비스'),
+  (2, 'https://naver.com', '/images/golf.jpg', '프리미엄 골프용품 서비스'),
+  (3, 'https://naver.com', '/images/preehimo.jpg', '프리하이모 가발 패키지 서비스'),
+  (4, 'https://naver.com', '/images/tour.jpg', '여행 서비스'),
+  (5, 'https://naver.com', '/images/wedding.jpg', '웨딩 서비스'),
+  (6, 'https://naver.com', '/images/golf_package.jpg', '골프 패키지'),
+  (7, 'https://naver.com', '/images/baby.jpg', '돌잔치 서비스'),
+  (8, 'https://naver.com', '/images/lang.jpg', '어학연수 서비스'),
+  (9, 'https://naver.com', '/images/parents.jpg', '수연 서비스'),
+  (10, 'https://naver.com', '/images/hyundairibart.jpg', '현대 리바트 홈 인테리어 서비스'),
+  (11, 'https://naver.com', '/images/ceragem_home.jpg', '세라잼 홈 헬스케어 서비스'),
+  (12, 'https://naver.com', '/images/ground.jpg', '장지 서비스');
