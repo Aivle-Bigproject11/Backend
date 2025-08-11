c301010d964143822ae877b2e3408c27424727e5
minseo <kmseo0707@naver.com>
2025-08-11 17:12:55 +0900

    application.yml 다 수정

--- funeralcontext/src/main/java/aivlebigproject/config/WebConfig.java
+++ funeralcontext/src/main/java/aivlebigproject/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000","http://localhost:8082", "http://localhost:8088") // 모든 도메인 허용
+                .allowedOrigins("http://localhost:3000","http://localhost:8080","http://localhost:8083", "http://localhost:8088") // 모든 도메인 허용
                 .allowedMethods("*") // 허용할 HTTP method
                 .allowedHeaders("*")        // 모든 헤더 허용
                 .allowCredentials(true) // 쿠키 인증 요청 허용
