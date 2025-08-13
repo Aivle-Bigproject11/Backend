df92b3d76e391be3edc7c746e5046ec419893208
godhyunjong <102642193+godhyunjong@users.noreply.github.com>
2025-08-13 14:10:14 +0900

    Update CustomerInfoViewHandler.java
    250813_14:10_현종

--- servicecontext/src/main/java/aivlebigproject/infra/CustomerInfoViewHandler.java
+++ servicecontext/src/main/java/aivlebigproject/infra/CustomerInfoViewHandler.java
@@ -10,8 +10,6 @@ import org.springframework.transaction.annotation.Transactional;
 //import javax.transaction.Transactional;
 import java.util.Optional;
 
-import java.util.Optional;
-
 @Service
 public class CustomerInfoViewHandler {
 
@@ -82,49 +80,4 @@ public class CustomerInfoViewHandler {
             customerInfoRepository.deleteById(e.getCustomerId());
         }
     }
-
-    @StreamListener(KafkaProcessor.INPUT)
-    public void whenCustomerUpdated_then_UPDATE_1(
-            @Payload CustomerUpdated customerUpdated
-    ) {
-        try {
-            if (!customerUpdated.validate()) return;
-            // view 객체 조회
-            Optional<CustomerInfo> customerInfoOptional = customerInfoRepository.findById(
-                    customerUpdated.getCustomerId()
-            );
-
-            if (customerInfoOptional.isPresent()) {
-                CustomerInfo customerInfo = customerInfoOptional.get();
-                // view 객체에 이벤트의 eventDirectValue 를 set 함
-                customerInfo.setAge(customerUpdated.getAge());
-                customerInfo.setName(customerUpdated.getName());
-                customerInfo.setPhone(customerUpdated.getPhone());
-                customerInfo.setJob(customerUpdated.getJob());
-                customerInfo.setAddress(customerUpdated.getAddress());
-                customerInfo.setGender(customerUpdated.getGender());
-                customerInfo.setBirthDate(customerUpdated.getBirthDate());
-                customerInfo.setHasChildren(customerUpdated.getHasChildren());
-                customerInfo.setIsMarried(customerUpdated.getIsMarried());
-                // view 레파지 토리에 save
-                customerInfoRepository.save(customerInfo);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @StreamListener(KafkaProcessor.INPUT)
-    public void whenCustomerDeleted_then_DELETE_1(
-            @Payload CustomerDeleted customerDeleted
-    ) {
-        try {
-            if (!customerDeleted.validate()) return;
-            // view 레파지 토리에 삭제 쿼리
-            customerInfoRepository.deleteById(customerDeleted.getCustomerId());
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    //>>> DDD / CQRS
 }
